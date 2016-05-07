acda9af311f7872648565e972c9e6b1cc6bba0e7
Scott Fisk <sdfisk@gmail.com>
2016-05-07 11:23:44 -0400

    updates to allow team subsets to be analyzed for relative power

--- magicrush.py
+++ magicrush.py
@@ -1,6 +1,8 @@
 import csv
-import pandas
 import numpy
+import pandas
+
+from itertools import combinations
 from sklearn import preprocessing
 
 
@@ -14,10 +16,10 @@ def main():
         data_iter = csv.DictReader(fp, delimiter=',')
         data = [r for r in data_iter]
 
-        all_heros = set()
-        # get list of heros
+        all_heroes = set()
+        # get list of heroes
         for team in data:
-            all_heros.update(members_as_list(team))
+            all_heroes.update(members_as_list(team))
 
         formatted_data = []
         for team in data:
@@ -26,7 +28,7 @@ def main():
                 'rank': int(team['Rank']),
                 'power': int(team['Power'])
             }
-            [formatted_team.update({hero: hero in members_as_list(team)}) for hero in all_heros]
+            [formatted_team.update({hero: hero in members_as_list(team)}) for hero in all_heroes]
             formatted_data.append(formatted_team)
 
         # for each server
@@ -49,16 +51,29 @@ def main():
 
         normed_data = pandas.concat(frames)
 
-        avg_power = []
-        for hero in all_heros:
-            # teams with this hero present
-            teams = normed_data[normed_data[hero]]['power']
-            power = teams.mean()
-            count = teams.count()
-            avg_power.append({'hero': hero, 'avg_power': power, 'count': count})
+        # spaces seem to anger pandas query command
+        normed_data.columns = [c.replace(' ', '_') for c in normed_data.columns]
+        all_heroes = [c.replace(' ', '_') for c in all_heroes]
 
-        for item in sorted(avg_power, key=lambda k: k['avg_power'], reverse=True):
-            print("{},{},{}".format(item['hero'], int(item['avg_power']), item['count']))
+        subset_sizes = [1, 2, 3, 4, 5]
+        for subset_size in subset_sizes:
+            avg_power = []
+            # iterates over all combinations of a team for a given size
+            for heroes in combinations(all_heroes, subset_size):
+                # teams with this hero present
+                exp = (' & ').join('({})'.format(h) for h in heroes)
+                teams = normed_data.query(exp)['power']
+                if len(teams) > 10:
+                    power = teams.mean()
+                    count = teams.count()
+                    avg_power.append({'heroes': ','.join(h for h in sorted(heroes)), 'avg_rel_power': int(power), 'count': count})
+
+            with open('team_size{}.csv'.format(subset_size), 'w') as csvfile:
+                fieldnames = ['heroes', 'avg_rel_power', 'count']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
+                # write out sorted by power
+                writer.writerows(sorted(avg_power, key=lambda k: k['avg_rel_power'], reverse=True))
 
 
 if __name__ == '__main__':
